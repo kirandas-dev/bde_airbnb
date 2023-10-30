0bbeb76f1570366f89cb4252d72e7f06b62c25b5
kirandas-dev <KiranShanker.Das@student.uts.edu.au>
2023-10-30 19:34:06 +1100

    Staging Layer Fixes

---
+++ models/staging/stg_G02.sql
@@ -0,0 +1,19 @@
+{{
+    config(
+      target_schema='staging'
+    
+       )
+}}
+
+with source as (
+    select *
+    from {{ source('public', '2016Census_G02_NSW_LGA') }}
+),
+stg_G02 as (
+   SELECT CAST(substring(lga_code_2016 FROM 4) AS INTEGER) AS lga_code, median_age_persons, median_mortgage_repay_monthly, median_tot_prsnl_inc_weekly, median_rent_weekly, median_tot_fam_inc_weekly, average_num_psns_per_bedroom, median_tot_hhd_inc_weekly, average_household_size
+
+FROM source
+
+)
+select * from stg_G02
+
